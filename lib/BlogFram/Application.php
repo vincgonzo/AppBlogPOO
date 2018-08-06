c6feb4556c5e541236ac6bd7090885e3b170b244
vincent-castanie <vincgonzo@hotmail.fr>
2018-08-06 15:40:24 +0200

    getController method \Application

--- lib/BlogFram/Application.php
+++ lib/BlogFram/Application.php
@@ -1,5 +1,5 @@
 <?php
-namespace OCFram;
+namespace BlogFram;
 
 abstract class Application
 {
@@ -13,6 +13,41 @@ abstract class Application
         $this->httpResponse = new HTTPResponse;
         $this->name = '';
     }
+
+    public function getController()
+    {
+        $router = new Router();
+
+        $xml = new \DOMDocument();
+        $xml->load(__DIR__. '/../../Config/routes.xml');
+
+        $routes = $xml->getElementsByTagName('route');
+
+        foreach ($routes as $route) {
+            $vars = [];
+
+            if($route->hasAttribute('vars')){
+                $vars = explode(',', $route->getAttribute('vars'));
+            }
+
+            $router->addRoute(new Route($route->getAttribute('url'), $route->getAttribute('module'), $route->getAttribute('action'), $vars));
+        }
+
+        try {
+           $matchedRoute = $router->getRoute($this->httpRequest->requestURI());
+        } catch (Exception $e) {
+          if($e->getCode() == Router::NO_ROUTE)
+          {
+              $this->httpResponse->redirect404();
+          }
+        }
+
+        $_GET = array_merge($_GET, $matchedRoute->vars());
+        $controllerClass = '\\Modules\\'.$matchedRoute->module().'\\'.$matchedRoute->module().'Controller';
+
+        return new $controllerClass($this, $matchedRoute->module(), $matchedRoute->action());
+    }
+
 
     abstract public function run();
 
