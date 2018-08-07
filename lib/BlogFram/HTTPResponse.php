284b985beadf0c9103de64814ff1a471ad93617a
vincent-castanie <vincgonzo@hotmail.fr>
2018-08-07 10:29:50 +0200

    404 request

--- lib/BlogFram/HTTPResponse.php
+++ lib/BlogFram/HTTPResponse.php
@@ -18,7 +18,12 @@ class HTTPResponse extends ApplicationComponent
 
     public function redirect404()
     {
+        $this->page = new Page($this->app);
 
+        $this->page->setContentFile(__DIR__.'/../../Errors/404.html');
+        $this->addHeader('HTTP/1.0 404 Not Found');
+
+        $this->send();
     }
 
     public function send()
