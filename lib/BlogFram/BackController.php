6c6e7fc8795a1eafbb0828f44472ed9cd17c4487
vincent-castanie <vincgonzo@hotmail.fr>
2018-08-08 11:17:39 +0200

    code review (2)

--- lib/BlogFram/BackController.php
+++ lib/BlogFram/BackController.php
@@ -67,6 +67,6 @@ abstract class BackController extends ApplicationComponent
 
         $this->view = $view;
 
-        $this->page->setContentFile(__DIR__.'../../App/'.$this->app->name().'/Modules/'. $this->module . '/Views/'. $this->view . '.php');
+        $this->page->setContentFile(__DIR__.'/../../App/'.$this->app->name().'/Modules/'. $this->module . '/Views/'. $this->view . '.php');
     }
 }
